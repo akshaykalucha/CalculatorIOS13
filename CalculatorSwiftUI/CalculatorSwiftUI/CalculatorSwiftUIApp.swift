df487b90218497a98d79bf9fd06c355377cf676b
akshaykalucha3 <akshaykalucha@gmail.com>
2022-02-17 16:08:43 +0530

    SwiftUI Calculator

---
+++ CalculatorSwiftUI/CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Akshay Kalucha on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorSwiftUI/CalculatorSwiftUI/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  CalculatorSwiftUI
+//
+//  Created by Akshay Kalucha on 16/02/22.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("Hello, World!").padding(EdgeInsets(top: 500, leading: 0, bottom: 0, trailing: 0))
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
